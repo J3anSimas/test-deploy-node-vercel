9bafa8cc6886260ecdfb92af00f1f85be76d180a
Jean Simas <jeansimas16@gmail.com>
2022-11-30 15:09:31 -0300

    get form data

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,17 @@
 require('dotenv').config()
-const fs = require('fs')
+const bodyParser = require('body-parser')
 const { twiml } = require('twilio')
 const express = require('express')
 const PORT = process.env.PORT || 3000
 const server = express()
-
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true }))
 
 server.post('/api', (request, response) => {
   const data = JSON.stringify(request.body)
 
   // fs.writeFileSync('request.txt', request.body.toString())
-  console.log(request.headers);
+  console.log(request.body);
   const responseTwilio = new twiml.MessagingResponse()
   const t = responseTwilio.message('Voce acabou de enviar uma msg')
   console.log("teste");
