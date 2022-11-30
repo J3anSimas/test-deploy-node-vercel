53e0bcd0941f2bc42f099a67ab4bd6db942fb1c1
Jean Simas <jeansimas16@gmail.com>
2022-11-30 14:59:57 -0300

    trying to get params

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-
+const fs = require('fs')
 const { twiml } = require('twilio')
 const express = require('express')
 const PORT = process.env.PORT || 3000
@@ -7,7 +7,10 @@ const server = express()
 
 
 server.post('/api', (request, response) => {
-  console.log(request)
+  const data = JSON.stringify(request.body)
+
+  // fs.writeFileSync('request.txt', request.body.toString())
+  console.log(request.params);
   const responseTwilio = new twiml.MessagingResponse()
   const t = responseTwilio.message('Voce acabou de enviar uma msg')
   console.log("teste");
