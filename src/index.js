aaea58b990d878c83eff3c3701e1b655087ee10e
Jean Simas <jeansimas16@gmail.com>
2022-11-30 10:12:27 -0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const fastify = require('fastify')
+const PORT = process.env.PORT || 3000
+const server = fastify({
+  logger: true
+})
+
+server.get('/', (request, response) => {
+  return 'Funcionando'
+})
+
+
+server.listen({ port: PORT }, (err, address) => {
+  if (err) {
+    server.log.error(err)
+    process.exit(1)
+  }
+  server.log.info(`Server running on ${address}`)
+})
